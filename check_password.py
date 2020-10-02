1c44fbb3094273a339625cdf794345a9df94c060
MrMohammadY <m.yazdani000@gmail.com>
2020-10-02 21:08:27 +0330

    adding comment and pep8

--- check_password.py
+++ check_password.py
@@ -1,9 +1,11 @@
 from string import punctuation
+
+# these variables are for color
 BOLD, RED, YELLOW, GREEN, BLUE = '\033[1m', '\033[31m', '\033[93m',\
                                  '\033[32m', '\033[34m'
 
 
-def show_result(password, result):
+def show_result(password, result):  # this function only levels and prints them
     count_true_result = result.count('True')
     grade = '==='
 
@@ -11,21 +13,24 @@ def show_result(password, result):
         print(f' {BOLD}Your Password is : {BLUE}{password}\n'
               f' {RED}{grade}'
               f' \n This password is VeryShort !!!!!!!!')
+
     if count_true_result == 1:
         print(f' {BOLD}Your Password is : {BLUE}{password}\n'
               f' {RED}{grade}'
               f' \n This password is Week !')
+
     if count_true_result == 2:
         print(f' {BOLD}Your Password is : {BLUE}{password}\n'
               f' {RED}{grade}{YELLOW}{grade}'
               f' \n This password is Medium :)')
+
     if count_true_result >= 3:
         print(f' {BOLD}Your Password is : {BLUE}{password}\n'
               f' {RED}{grade}{YELLOW}{grade}{GREEN}{grade}'
               f' \n This password is Strong :D')
 
 
-def recognize_password_strength(password):
+def recognize_password_strength(password):  # this func take pass and check it
     result_check = str()
 
     if len(password) < 8:
