19e1832f3af7855f905a66f504a403ab12e9dd39
MrMohammadY <m.yazdani000@gmail.com>
2020-10-02 20:48:53 +0330

    add recognizing of a password strength

--- check_password.py
+++ check_password.py
@@ -1,5 +1,43 @@
+from string import punctuation
+BOLD, RED, YELLOW, GREEN, BLUE = '\033[1m', '\033[31m', '\033[93m',\
+                                 '\033[32m', '\033[34m'
 
-#TODO 3    Create a Password strenghth meter
-#یک فانکشن بنویسید که کاربرد بتونه رشته رو به عنوان پسورد وارد کنه و در خروجی ببینه که پسوردش چقدر قهوی هستش
-#دسته باندی پسوردها رو به سه دسته قوی - متوسط و ضعیف تقسیم کنید. 
-#اینکه چطور بتونه تشخیص بده که پسورد چقدر قویه، میتونه برا اساس فاتورهای مختلف باشه که سلیقه خودتونه
+
+def show_result(password, result):
+    count_true_result = result.count('True')
+    grade = '==='
+
+    if count_true_result == 0:
+        print(f' {BOLD}Your Password is : {BLUE}{password}\n'
+              f' {RED}{grade}'
+              f' \n This password is VeryShort !!!!!!!!')
+    if count_true_result == 1:
+        print(f' {BOLD}Your Password is : {BLUE}{password}\n'
+              f' {RED}{grade}'
+              f' \n This password is Week !')
+    if count_true_result == 2:
+        print(f' {BOLD}Your Password is : {BLUE}{password}\n'
+              f' {RED}{grade}{YELLOW}{grade}'
+              f' \n This password is Medium :)')
+    if count_true_result >= 3:
+        print(f' {BOLD}Your Password is : {BLUE}{password}\n'
+              f' {RED}{grade}{YELLOW}{grade}{GREEN}{grade}'
+              f' \n This password is Strong :D')
+
+
+def recognize_password_strength(password):
+    result_check = str()
+
+    if len(password) < 8:
+        result_check = 'False'
+    else:
+        result_check += f'{any(char.islower() for char in password)}'
+        result_check += f'{any(char.isupper() for char in password)}'
+        result_check += f'{any(char.isdigit() for char in password)}'
+        result_check += f'{any(char in punctuation for char in password)}'
+
+    show_result(password, result_check)
+
+
+pass_user = input(f'{BLUE}Please enter Your password to check : ')
+recognize_password_strength(pass_user)
